650deeb078037b10baebaf466c028786cd439da1
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-01-03 20:48:51 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (5.12%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int ans=0;
+    //    unordered_map<char,int>m1;
+    // //    for(int i=0;i<jewels.size();i++){
+    // //        m1[jewels[i]]++;
+    // //    }
+       unordered_map<char,int>m2;
+       for(int i=0;i<stones.size();i++){
+           m2[stones[i]]++;
+       }
+    //    for(int i=0;i<max(jewels.size(),stones.size());i++){
+    //        if(m2[jewels[i]]){
+    //            ans=ans+m2[jewels[i]];
+    //        }
+    //    }
+    int n=jewels.size();
+    for(auto i:jewels){
+        if(m2[i]){
+            ans+=m2[i];
+        }
+    }
+        return ans;
+    }
+};
