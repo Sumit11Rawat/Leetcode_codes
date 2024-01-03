f707e8fa9db87b0fd4adec102ee26ee1436d04d3
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-01-03 20:14:20 +0530

    Time: 10 ms (60.25%) | Memory: 10.2 MB (45.37%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int maxi=INT_MIN;
+      
+        for(int i=0;i<sentences.size();i++){
+              int ans=0;
+            for(int j=0;j<sentences[i].size();j++){
+                if(sentences[i][j]==' '){
+                    ans++;
+                }
+            }
+            maxi=max(maxi,ans);
+        }
+        return maxi+1;
+    }
+};
