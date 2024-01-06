f5916adfcc95aebc0c8b005c6d5fce4bb0152ab4
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-01-06 09:16:53 +0530

    Time: 10 ms (70.38%) | Memory: 16.7 MB (68.48%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            nums[i]=(nums[nums[i]]%nums.size())*nums.size()+nums[i];
+        }
+        for(int i=0;i<nums.size();i++){
+            nums[i]/=nums.size();
+        }
+       
+        return nums;
+    }
+};
