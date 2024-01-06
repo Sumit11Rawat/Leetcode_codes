c0907a4d1041b49baac3bb516c752773f2eceb56
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-01-06 11:44:16 +0530

    Time: 6 ms (84.46%) | Memory: 10.8 MB (19.21%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -2,18 +2,33 @@ class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
     //    brute force
-    vector<int>ans;
-    for(int i=0;i<nums.size();i++){
-        int anss=0;
-     for(int j=0;j<nums.size();j++){
-         if(nums[i]>nums[j]){
-            anss++;
-         }
+    // vector<int>ans;
+    // for(int i=0;i<nums.size();i++){
+    //     int anss=0;
+    //  for(int j=0;j<nums.size();j++){
+    //      if(nums[i]>nums[j]){
+    //         anss++;
+    //      }
          
 
-     }
-     ans.push_back(anss);
+    //  }
+    //  ans.push_back(anss);
+    // }
+    // return ans;
+    vector<int>ans=nums;
+sort(ans.begin(),ans.end());
+unordered_map<int,int>m;
+for(int i=nums.size()-1; i>=0; i--) {
+            m[ans[i]] = i;
+        }
+for(int i=0;i<nums.size();i++){
+  nums[i]=m[nums[i]];
+}
+return nums;
     }
-    return ans;
-    }
-};
+}; 
+// 4568
+// 6-0
+// 5-1
+// 4-2
+// 8-3
