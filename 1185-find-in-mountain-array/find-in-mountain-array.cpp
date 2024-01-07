3434e35af0aad3680404ef4ae25322dc5577bca8
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-01-07 19:48:29 +0530

    Time: 6 ms (41.01%) | Memory: 7.4 MB (27.25%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.cpp
@@ -0,0 +1,70 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &arr) {
+        if(arr.length()<3){
+            return -1;
+        }
+           int i=0;
+        int ans=0;
+        int j=arr.length()-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr.get(mid)>arr.get(mid+1)&&arr.get(mid)>arr.get(mid-1)){
+                ans=mid;
+                break;
+            }
+            else if(arr.get(mid)<arr.get(mid+1)){
+                i=mid+1;
+            }
+            else{
+               j=mid-1;
+            }
+        }
+         i=0;
+         j=ans;
+        
+        while(i<=j){
+           int midd=i+(j-i)/2;
+           int l=arr.get(midd);
+            if(l==target){
+                return midd;
+            }
+            else if(l>target){
+                j=midd-1;
+            }
+            else{
+                i=midd+1;
+            }
+        }
+        i=ans+1;
+         j=arr.length()-1;
+        
+        while(i<=j){
+           int midd=i+(j-i)/2;
+           int l=arr.get(midd);
+            if(l==target){
+                return midd;
+            }
+            else if(l>target){
+              
+                  i=midd+1;
+            }
+            else{
+                j=midd-1;
+            }
+        }
+return -1;
+    }
+     
+};
+// 1 2 3 4 5 
