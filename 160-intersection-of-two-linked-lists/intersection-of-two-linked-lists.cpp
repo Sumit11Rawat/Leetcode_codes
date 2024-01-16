aaee18e12d6754c4d187753b74e33c0787137c9b
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-01-16 20:48:27 +0530

    Time: 40 ms (49.37%) | Memory: 14.9 MB (51.76%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,26 +8,65 @@
  */
 class Solution {
 public:
+int length(ListNode* head){
+    int len=0;
+    while(head!=NULL){
+        len++;
+        head=head->next;
+    }
+    return len;
+}
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode*,int>m;
-        ListNode* temp=headA;
-        while(temp!=NULL){
-            m[temp]++;
-            temp=temp->next;
-        }
-       
-       temp=headB;
-       ListNode* ftemp=NULL;
-       while(temp!=NULL){
-           if(m[temp]){
-               ftemp=temp;
-               break;
+        // brute force -using hashmap
 
-           }
-           else{
-               temp=temp->next;
+    //     unordered_map<ListNode*,int>m;
+    //     ListNode* temp=headA;
+    //     while(temp!=NULL){
+    //         m[temp]++;
+    //         temp=temp->next;
+    //     }
+       
+    //    temp=headB;
+    //    ListNode* ftemp=NULL;
+    //    while(temp!=NULL){
+    //        if(m[temp]){
+    //            ftemp=temp;
+    //            break;
+
+    //        }
+    //        else{
+    //            temp=temp->next;
+    //        }
+    //    }
+    //    return ftemp;
+
+    // optimal approach
+    ListNode* temp=NULL;
+       int len1=length(headA);
+       int len2=length(headB);
+       if(len1>len2){
+           while(len1>len2){
+               headA=headA->next;
+               len1--;
            }
        }
-       return ftemp;
+       else{
+           while(len2>len1){
+               headB=headB->next;
+               len2--;
+           }
+       }
+       while(headA!=NULL&&headB!=NULL){
+             if(headA==headB){
+                 temp=headA;
+                 break;
+             }
+             else{
+                 headA=headA->next;
+                 headB=headB->next;
+             }
+       }
+       return temp;
+       
     }
 };
