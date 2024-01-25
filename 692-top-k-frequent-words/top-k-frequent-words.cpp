65ac1fb6f8daaae32a5a134ff42cda30be4a7117
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-01-25 22:00:32 +0530

    Time: 12 ms (48.92%) | Memory: 16.5 MB (9.66%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -8,38 +8,31 @@ static bool compare(pair<int, string>& a, pair<int, string>& b){
     //else compare on the basis of their freq
     else return a.first > b.first;
 }
-
-vector<string> topKFrequent(vector<string>& words, int k) {
-    int n = words.size();
-    
-    //use a map to store the freq of all the words from the vector
-    unordered_map<string, int> mp;
-    
-    for(auto x: words){
-        mp[x]++;
-    }
-    
-    //store the words with their freq in a vector to sort them
-    vector<pair<int, string>> v;
-    
-    for(auto x: mp){
-        v.push_back({x.second, x.first});
-    }
-    
-    //sort the vector using the above defined compare function
-    sort(v.begin(), v.end(), compare);
-    
-    //put only k elements in the ans vector
-    vector<string> ans;
-    for(int i=0;i<v.size();i++)
-    {
-        if(k)
-        {
-            ans.push_back(v[i].second);
-            k--;
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int>m;
+        for(auto i:words){
+            m[i]++;
         }
+        vector<pair<int,string>>p;
+        for(auto it:m){
+            p.push_back(make_pair(it.second,it.first));
+        }
+        sort(p.begin(),p.end(),compare);
+        // reverse(p.begin(),p.end());
+        vector<string>ans;
+       for(auto it:p){
+            if(k==0){
+                break;
+                
+            }
+            else{
+                
+                ans.push_back(it.second);
+                k--;
+            }
+        }
+    //  sort(ans.begin(),ans.end());
+        return ans;
+        
     }
-    
-    return ans;
-}
 };
