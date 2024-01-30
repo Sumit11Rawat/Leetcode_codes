63e94f7d4b5ba8a417bed597fb701f20695e7bc7
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-01-30 10:00:24 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (15.25%) - LeetSync

---
+++ 476-number-complement/number-complement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask=1;
+        while((mask&num)!=num){
+            mask=mask<<1;
+            mask=mask|1;
+        }
+        return mask^num;
+    }
+};
