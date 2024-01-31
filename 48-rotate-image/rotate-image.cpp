573d038960b279445815b5bd71d7cb5d838d627c
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-01-31 10:04:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (12.65%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        // take transpose
         for(int i=0;i<matrix.size();i++){
             for(int j=i+1;j<matrix.size();j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-       
+    //    swap column with middle respect to middle column 
        for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix.size()/2;j++){
                 swap(matrix[i][j], matrix[i][matrix.size()-j-1]);
