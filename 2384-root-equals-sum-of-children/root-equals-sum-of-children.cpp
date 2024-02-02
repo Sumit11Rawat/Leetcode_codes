f0b55aa6e49bc47029474ea3accac811f5826768
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-02-02 14:17:34 +0530

    Time: 4 ms (21.40%) | Memory: 14.6 MB (5.09%) - LeetSync

--- 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
@@ -12,10 +12,11 @@
 class Solution {
 public:
     bool checkTree(TreeNode* root) {
-        int sum=root->left->val+root->right->val;
-        if(sum==root->val){
-            return true;
-        }
-        return false;
+        // int sum=root->left->val+root->right->val;
+        // if(sum==root->val){
+        //     return true;
+        // }
+        // return false;
+        return root->left->val+root->right->val==root->val;
     }
 };
