24e5df8a407c72224f9d9d8f1d2e14f76df6e5e0
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-02-06 08:39:59 +0530

    Time: 22 ms (92.50%) | Memory: 24.2 MB (28.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-       unordered_map<string,vector<string>>m;
-       for(auto c:strs){
-           string ans=c;
-           sort(c.begin(),c.end());
-           m[c].push_back(ans);
+        unordered_map<string,vector<string>>m;
+        for(auto i:strs){
+            string c=i;
+            sort(i.begin(),i.end());
+            m[i].push_back(c);
+        }
+        vector<vector<string>>fans;
+        for(auto i:m){
+            fans.push_back(i.second);
 
-       }
-       vector<vector<string>>fans;
-       for(auto it:m){
-           fans.push_back(it.second);
-       }
-       return fans;
+        }
+        return fans;
     }
 };
