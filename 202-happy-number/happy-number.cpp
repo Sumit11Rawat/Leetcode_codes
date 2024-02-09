38940f72094c44fb43ee6b41f3b4dc13aa712328
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-02-09 13:15:18 +0530

    Time: 6 ms (6.65%) | Memory: 12.2 MB (5.13%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    bool chk(int n,unordered_map<int,bool>m){
+    bool chk(int n,unordered_map<int,int>m){
         if(n==1){
             return true;
         }
-       if(n==4){
-           return false;
-       }
-        else{
+    //    if(n==4){
+    //        return false;
+    //    }
+    if(m[n]>0){
+        return false;
+    }
+    else{
+        m[n]++;
+    }
+         
             int ans=0;
             while(n){
                 ans+=(n%10)*(n%10);
                 n=n/10;
             }
-            m[ans]=true;
+            // m[ans]++;
             return chk(ans,m);
-        }
+        
         
     //    return false;
     }
     bool isHappy(int n){
         // int a=n;
-        unordered_map<int,bool>m;
-        m[n]=true;
+        unordered_map<int,int>m;
+        // m[n]++;
          if(chk(n,m)){
              return true;
          }
