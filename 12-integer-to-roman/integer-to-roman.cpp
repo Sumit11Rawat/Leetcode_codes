716a4e111dce99aff067a25a346383a3ea6fc8e1
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-02-19 20:29:01 +0530

    Time: 7 ms (59.71%) | Memory: 13.4 MB (16.07%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ans="";
+        vector<pair<int,string>>p{{1,"I"},{4,"IV"},{5,"V"},{9,"IX"},{10,"X"},{40,"XL"},{50,"L"},{90,"XC"},{100,"C"},{400,"CD"},{500,"D"},{900,"CM"},{1000,"M"}};
+        int l=p.size()-1;
+        
+        while(num>0){
+             while(p[l].first>num){
+                 l--;
+             }
+             ans+=p[l].second;
+             num=num-p[l].first;
+             
+        }
+         
+        return ans;
+
+    }
+};
