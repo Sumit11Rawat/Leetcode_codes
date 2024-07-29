239914e52c0a1c966fe14cfc027098aaab2eddf8
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-07-29 20:41:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (8.25%) - LeetSync

--- 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.cpp
+++ 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     string losingPlayer(int x, int y) {
-          for(int i=0;i<x;i++){
-            if(y>=4){
-                y-=4;
+        //   for(int i=0;i<x;i++){
+        //     if(y>=4){
+        //         y-=4;
+        //     }
+        //     else{
+        //         if(i%2==1){
+                 
+        //                return "Alice";
+        //         }
+        //         else{
+        //             return "Bob";
+        //         }
+        //     }
+        //   }
+        //   if(x%2==1){
+        //     return "Alice";
+        //   }
+        //   return "Bob";
+        y=4*(y/4);
+        if(4*x>y){
+           if((y/4)%2==1){
+            return "Alice";
+           }
+           else{
+            return "Bob";
+           }
+        }
+        else{
+            if(x%2==1){
+                return "Alice";
             }
             else{
-                if(i%2==1){
-                 
-                       return "Alice";
-                }
-                else{
-                    return "Bob";
-                }
+                return "Bob";
             }
-          }
-          if(x%2==1){
-            return "Alice";
-          }
-          return "Bob";
+        }
       
     }
 };
