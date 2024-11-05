a8fa1e5c5c23c77885a62e908627a8c71aa6c11f
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-11-05 08:08:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (16.88%) - LeetSync

---
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minChanges(string s) {
+
+        int count=0;
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i]!=s[i+1]){
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+};
