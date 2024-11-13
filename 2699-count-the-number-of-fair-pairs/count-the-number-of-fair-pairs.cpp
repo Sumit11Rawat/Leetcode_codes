afe088880aba537470795bd15aca9ada3778ebed
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-11-13 19:02:36 +0530

    Time: 74 ms (17.26%) | Memory: 60.3 MB (80.83%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -9,8 +9,8 @@ public:
         long long count=0;
         for(int i=0;i<nums.size();i++){
         
-            long long ind1=lower_bound(nums.begin()+i+1,nums.end(),lower-nums[i])-nums.begin();
-            long long ind2=upper_bound(nums.begin()+i+1,nums.end(),upper-nums[i])-nums.begin();
+            long long ind1=lower_bound(nums.begin()+i+1,nums.end(),lower-nums[i])-nums.begin()-i-1;
+            long long ind2=upper_bound(nums.begin()+i+1,nums.end(),upper-nums[i])-nums.begin()-i-1;
                 count+=ind2-ind1;
 
         }
@@ -19,3 +19,8 @@ public:
         
     }
 };
+// -50 50  100
+// -50 -10 40
+// 50 -80
+
+// -50 66 70
