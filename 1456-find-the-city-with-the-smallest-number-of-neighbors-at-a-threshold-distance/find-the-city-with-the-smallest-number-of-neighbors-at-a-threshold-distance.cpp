4207a0db1ccc3f0174b091a095d06226cada40fa
SUMIT RAWAT <136540491+Sumit11Rawat@users.noreply.github.com>
2024-12-07 07:52:30 +0530

    Time: 24 ms (26.12%) | Memory: 19.6 MB (19.62%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -13,65 +13,47 @@ public:
         return -1;
      }
         while(!pq.empty()){
+
             auto it=pq.top();
-            pq.pop();
-            
+            pq.pop();   
             int node=it.second;
-            cout<<"node :"<<node<<endl;
             int coming_wt=it.first;
+
             for(auto i:adj[node]){
+
                 int new_node=i.first;
                 int new_wt=i.second;
-                if(new_wt+coming_wt<fdist[new_node]){
-                    cout<<new_node<<" "<<coming_wt<<" "<<new_wt<<endl;
+
+                if(new_wt+coming_wt<fdist[new_node] and new_wt+coming_wt<=thresh){
                     fdist[new_node]=new_wt+coming_wt;
                     pq.push({fdist[new_node],new_node});
                 }
+
             }
             
         }
 
-        for(int i=0;i<adj.size();i++){
-            cout<<fdist[i]<<" ";
-        }
-        cout<<endl;
-
-     
      int count=0;
      for(int i=0;i<adj.size();i++){
         if(fdist[i]<=thresh){
             count++;
         }
      }
-     
      return count;
-
 
    }
     int findTheCity(int n, vector<vector<int>>& edges, int thresh) {
+
         vector<vector<pair<int,int>>>adj(n);
         for(int i=0;i<edges.size();i++){
             adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
             adj[edges[i][1]].push_back({edges[i][0],edges[i][2]});
         }
-        for(int i=0;i<n;i++){
-           cout<<i<<endl;
-           for(int j=0;j<adj[i].size();j++){
-            cout<<adj[i][j].first<<" "<<adj[i][j].second<<endl;;
-           }
-
-        }
+       
         vector<int>dist(n,1e9);
         for(int i=0;i<n;i++){
-            cout<<"i :"<<i<<endl;
             dist[i]=dijkstra(adj,i,thresh)-1;
-            cout<<dist[i]<<endl;
         }
-          for(int i=0;i<adj.size();i++){
-            cout<<dist[i]<<" ";
-        }
-        cout<<endl;
-
         
         int mini=1e9;
         for(int i=0;i<n;i++){
